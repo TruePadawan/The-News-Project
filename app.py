0c8859af68ab7e577012fb95d11632650e0f86e6
TruePadawan <hermeschigoziri@gmail.com>
2021-08-28 21:55:23 +0100

    Version 1.0
    Done with the Project.
    Removed the Login and Register Feature due to it not being neccesary

--- app.py
+++ app.py
@@ -66,56 +66,55 @@ def search():
 		data = searchNews(search)
 		session["pages"] = int(data["totalResults"]/10)
 		data = data["articles"]
-		print(data)
 		return render_template("search.html", data=data)
 
-@app.route('/login', methods=["GET", "POST"])
-def login():
-	## LOGIN THE USER
+# @app.route('/login', methods=["GET", "POST"])
+# def login():
+# 	## LOGIN THE USER
 
-	session.clear()
+# 	session.clear()
 
-	if request.method == "POST":
-		with sql.connect("project.db") as db:
-			db.row_factory = sql.Row
-			cur = db.cursor()
-			cur.execute("SELECT * FROM users WHERE name = ?",[request.form['username']])
-			rows = cur.fetchall()
-			if len(rows) == 0 or not check_password_hash(rows[0]["password"], request.form.get("password")):
-				return render_template("login.html", msg="No Account with registered with that Username and Password")
-			else:
-				session["user_id"] = rows[0]["id"]
-				return redirect('/')
-	else:
-		return render_template("login.html")
+# 	if request.method == "POST":
+# 		with sql.connect("project.db") as db:
+# 			db.row_factory = sql.Row
+# 			cur = db.cursor()
+# 			cur.execute("SELECT * FROM users WHERE name = ?",[request.form['username']])
+# 			rows = cur.fetchall()
+# 			if len(rows) == 0 or not check_password_hash(rows[0]["password"], request.form.get("password")):
+# 				return render_template("login.html", msg="No Account with registered with that Username and Password")
+# 			else:
+# 				session["user_id"] = rows[0]["id"]
+# 				return redirect('/')
+# 	else:
+# 		return render_template("login.html")
 
-@app.route('/register', methods=["GET", "POST"])
-def register():
-	#REGISTER AN ACCOUNT FOR THE USER
+# @app.route('/register', methods=["GET", "POST"])
+# def register():
+# 	#REGISTER AN ACCOUNT FOR THE USER
 
-	if request.method == "GET":
-		return render_template("register.html")
-	else:
-		username = request.form.get("username")
-		password = request.form.get("password")
-		pass_hash = generate_password_hash(password)
-		with sql.connect("project.db") as db:
-			cur = db.cursor()
-			cur.execute("SELECT * FROM users WHERE name = ?", [username])
-			row = cur.fetchall()
-			if len(row) != 0:
-				return render_template("register.html",msg="Account with Username already exists!")
-			cur.execute("INSERT INTO users (name,password) VALUES (?,?)",(username,pass_hash))
-			return redirect("/")
+# 	if request.method == "GET":
+# 		return render_template("register.html")
+# 	else:
+# 		username = request.form.get("username")
+# 		password = request.form.get("password")
+# 		pass_hash = generate_password_hash(password)
+# 		with sql.connect("project.db") as db:
+# 			cur = db.cursor()
+# 			cur.execute("SELECT * FROM users WHERE name = ?", [username])
+# 			row = cur.fetchall()
+# 			if len(row) != 0:
+# 				return render_template("register.html",msg="Account with Username already exists!")
+# 			cur.execute("INSERT INTO users (name,password) VALUES (?,?)",(username,pass_hash))
+# 			return redirect("/")
 
 
-@app.route("/logout")
-def logout():
-    # Forget any user_id
-    session.clear()
+# @app.route("/logout")
+# def logout():
+#     # Forget any user_id
+#     session.clear()
 
-    # Redirect user to login form
-    return redirect("/")
+#     # Redirect user to login form
+#     return redirect("/")
 
 if __name__=='__main__':
 	app.run(debug=True)
--- helpers.py
+++ helpers.py
@@ -23,8 +23,11 @@ def getNews():
     return news
 
 def searchNews(info):
+    before = date.today()
+    before = before.replace(month=(date.today().month - 1))
+    print(before)
     newsapi = NewsApiClient(api_key='e4d87e0f7a344caab7bb41c4f7318e84')
-    news = newsapi.get_everything(qintitle="+"+info, language='en', page_size=20, sort_by='relevancy', 
-    from_param=date.today(), to=date.today(), 
-    exclude_domains='theonlinecitizen.com,investmentwatchblog.com,protothema.gr,dailymail.co.uk,adweek.com,twit.tv,sankakucomplex.com,guru3d.com,news.com.au,developers.google.com,punchng.com,rappler.com,mb.com.ph,bbc.com,gmanetwork.com')
+    news = newsapi.get_everything(qintitle=info, language='en', page_size=20, 
+    from_param=before, to=date.today(), 
+    exclude_domains='theonlinecitizen.com,investmentwatchblog.com,protothema.gr,dailymail.co.uk,adweek.com,twit.tv,sankakucomplex.com,guru3d.com,news.com.au,developers.google.com,punchng.com,rappler.com,mb.com.ph,bbc.com,gmanetwork.com,fastcompany.com')
     return news
